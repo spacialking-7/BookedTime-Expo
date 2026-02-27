dfdbdcd8c3608015ecf1f60fe1ab198788b7c2dd
seth <sethgagne4@gmail.com>
2026-02-26 19:47:59 -0600

    root layout

--- app/_layout.js
+++ app/_layout.js
@@ -4,14 +4,23 @@ import TabsLayout from './(tabs)/_layout';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* Main Tabs */}
+      <Stack.Screen name="(tabs)/_layout" options={{ headerShown: false }} />
+      {/* Profile Stack */}
       <Stack.Screen name="profile/ProfileMain" options={{ title: 'Profile' }} />
       <Stack.Screen name="profile/EditProfile" options={{ title: 'Edit Profile' }} />
       <Stack.Screen name="achievements/index" />
       {/* Session stack */}
-      <Stack.Screen name="session/index" />
-      <Stack.Screen name="session/[id]" />
-      <Stack.Screen name="modal/add-session" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="session/index" options={{ title: 'Sessions' }} />
+      <Stack.Screen name="session/[id]" options={{ title: 'Session Details' }} />
+      {/* Modals */}
+       <Stack.Screen name="(modals)/add-session" options={{ presentation: 'modal', title: 'Add Session' }} />
+      <Stack.Screen name="(modals)/info" options={{ presentation: 'modal', title: 'Session Info' }} />
+   
     </Stack>
   );
 }
+
+
+
+
