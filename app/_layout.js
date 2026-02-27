945da9891f4852bddde715c2052335d1246c9962
seth <sethgagne4@gmail.com>
2026-02-26 20:08:43 -0600

    Changed title to "Booked Time" and added profile ("session/profile-stack") link to header.     -needs more details

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,24 @@
-import { Stack } from 'expo-router';
-import TabsLayout from './(tabs)/_layout';
+import { Stack, Link } from 'expo-router';
+import { Pressable, Text } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      {/* Main Tabs */}
-      <Stack.Screen name="(tabs)/_layout" options={{ headerShown: false }} />
-      {/* Profile Stack */}
-      <Stack.Screen name="profile/ProfileMain" options={{ title: 'Profile' }} />
-      <Stack.Screen name="profile/EditProfile" options={{ title: 'Edit Profile' }} />
-      <Stack.Screen name="achievements/index" />
-      {/* Session stack */}
-      <Stack.Screen name="session/index" options={{ title: 'Sessions' }} />
-      <Stack.Screen name="session/[id]" options={{ title: 'Session Details' }} />
-      {/* Modals */}
-       <Stack.Screen name="(modals)/add-session" options={{ presentation: 'modal', title: 'Add Session' }} />
-      <Stack.Screen name="(modals)/info" options={{ presentation: 'modal', title: 'Session Info' }} />
-   
-    </Stack>
+    <Stack
+      screenOptions={{
+        headerTitle: '',
+        headerRight: () => (
+          <Link href="/session/profile-stack" asChild>
+            <Pressable>
+              <Text style={{ fontSize: 16 }}>Profile</Text>
+            </Pressable>
+          </Link>
+        ),
+      }}
+    />
   );
 }
 
+          
 
 
 
