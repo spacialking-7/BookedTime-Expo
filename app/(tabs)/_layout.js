893a5bc516bad70aff146a9e041e0e06c287321f
seth <sethgagne4@gmail.com>
2026-02-26 19:36:48 -0600

    import (tabs)/_layout.js Added profile stack

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,11 +1,34 @@
 import { Tabs } from 'expo-router';
+import {Button} from 'react-native';
+import { useRouter } from 'expo-router'; // <-- Import router
 
 export default function TabsLayout() {
+  const router = useRouter(); // <-- Get router instance
+
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="history" options={{ title: "History" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen name="home" options={{ title: "Home",
+        headerRight: () => (<Button
+          title="Profile"
+          onPress={() => router.push('/profile/ProfileMain')}
+        />  
+        ) 
+      }} />
+      <Tabs.Screen name="history" options={{ title: "History",
+        headerRight: () => (<Button
+          title="Profile"
+          onPress={() => navigation.navigate('profile/ProfileMain')}
+        />  
+        )
+      }} />
+      
+      <Tabs.Screen name="achievements" options={{ title: "Achievements",
+        headerRight: () => (<Button
+          title="Profile"
+          onPress={() => navigation.navigate('profile/ProfileMain')}
+        />  
+        )
+      }} />
     </Tabs>
   );
 }
--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,14 @@
 import { Stack } from 'expo-router';
+import TabsLayout from './(tabs)/_layout';
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="profile/ProfileMain" options={{ title: 'Profile' }} />
+      <Stack.Screen name="profile/EditProfile" options={{ title: 'Edit Profile' }} />
       <Stack.Screen name="achievements/index" />
+      {/* Session stack */}
       <Stack.Screen name="session/index" />
       <Stack.Screen name="session/[id]" />
       <Stack.Screen name="modal/add-session" options={{ presentation: 'modal' }} />
